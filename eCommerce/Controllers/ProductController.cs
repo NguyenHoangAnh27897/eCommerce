3e8381fa403af49acd1d6a53290fe96fc9d6d34d
NguyenHoangAnh27897 <hoanganh27897@gmail.com>
2018-06-26 17:13:56 +0700

    init project

---
+++ eCommerce/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        public ActionResult SMSet()
+        {
+            return View();
+        }
+
+        public ActionResult SMAccessory()
+        {
+            return View();
+        }
+
+        public ActionResult VibTR()
+        {
+            return View();
+        }
+
+        public ActionResult VibMR()
+        {
+            return View();
+        }
+
+        public ActionResult AnalButt()
+        {
+            return View();
+        }
+
+        public ActionResult AnalBead()
+        {
+            return View();
+        }
+
+        public ActionResult TailFox()
+        {
+            return View();
+        }
+
+        public ActionResult TailDog()
+        {
+            return View();
+        }
+        public ActionResult Paddle()
+        {
+            return View();
+        }
+        public ActionResult Whip()
+        {
+            return View();
+        }
+        public ActionResult Cane()
+        {
+            return View();
+        }
+        public ActionResult FakeDildo()
+        {
+            return View();
+        }
+        public ActionResult BlockDildo()
+        {
+            return View();
+        }
+        public ActionResult Strapon()
+        {
+            return View();
+        }
+        public ActionResult BallGag()
+        {
+            return View();
+        }
+        public ActionResult RungGag()
+        {
+            return View();
+        }
+	}
+}
